045c82b3e69c302be58cd4e8f2ab814e4b2c65d5
Milan Shah <milanshah1@gmail.com>
2021-11-23 16:09:54 +0530

    setup prisma

--- plugins/prisma.js
+++ plugins/prisma.js
@@ -1,7 +1,9 @@
-import fp from 'fastify-plugin'
-import { PrismaClient } from '@prisma/client'
+const fp = require('fastify-plugin')
+const { PrismaClient } = require('@prisma/client')
 
-const prismaPlugin = fp(async (server, options) => {
+// import { PrismaClient } from '@prisma/client'
+
+module.exports = fp(async (server, options) => {
     const prisma = new PrismaClient()
     await prisma.$connect()
     // Make Prisma Client available through the fastify server instance: server.prisma
@@ -10,4 +12,3 @@ const prismaPlugin = fp(async (server, options) => {
         await server.prisma.$disconnect()
     })
 })
-export default prismaPlugin
