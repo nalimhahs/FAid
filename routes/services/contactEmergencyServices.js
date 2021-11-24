589c13e63e2028858e28fb41f2d20e3ae7e99c6c
Milan Shah <milanshah1@gmail.com>
2021-11-24 21:21:30 +0530

    finish emergency calling

--- routes/services/contactEmergencyServices.js
+++ routes/services/contactEmergencyServices.js
@@ -16,30 +16,52 @@ module.exports = async function (fastify, opts) {
             const session = await fastify.prisma.session.findUnique({
                 where: {
                     id: parseInt(sessionId, 10)
+                },
+                include: {
+                    symptoms: true,
                 }
             })
 
-            console.log(session, session.symptoms)
-
             const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${session.location}.json?limit=1&access_token=${process.env.MAPBOX_TOKEN}`
             const res = await axios.get(geocodeURL)
-            
-            const location = res.data.place_name
-            const symptoms = session.symptoms
+
+            const location = res.data.features[0].place_name
+            let symptoms = ''
+            session.symptoms.forEach((symptom) => {
+                symptoms += symptom.name + ', '
+            })
+
+            if (!symptoms) {
+                return ({ 'error': 'Invalid Symptoms' })
+            }
 
             switch (qSeverity) {
                 case Severity.POLICE:
+                    client.calls
+                        .create({
+                            twiml: `<Response><Say>Police help required at ${location}. Patient is ${symptoms}. Thank You</Say></Response>`,
+                            to: '+917356862872',
+                            from: '+13312416386'
+                        })
+                        .then(call => console.log(call.sid));
                     break;
                 case Severity.MEDICAL:
-                    // client.calls
-                    //     .create({
-                    //         twiml: `<Response><Say>Medical help required at ${location}. Patient is ${symptoms}. Thank You</Say></Response>`,
-                    //         to: '+917356862872',
-                    //         from: '+13312416386'
-                    //     })
-                    //     .then(call => console.log(call.sid));
+                    client.calls
+                        .create({
+                            twiml: `<Response><Say>Medical help required at ${location}. Patient is ${symptoms}. Thank You</Say></Response>`,
+                            to: '+917356862872',
+                            from: '+13312416386'
+                        })
+                        .then(call => console.log(call.sid));
                     break;
                 case Severity.BOTH:
+                    client.calls
+                        .create({
+                            twiml: `<Response><Say>Medical and police help required at ${location}. Patient is ${symptoms}. Thank You</Say></Response>`,
+                            to: '+917356862872',
+                            from: '+13312416386'
+                        })
+                        .then(call => console.log(call.sid));
                     break;
                 default:
                     reply.send({ 'error': 'Invalid Severity' })
